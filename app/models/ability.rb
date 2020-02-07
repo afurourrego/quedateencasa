d91e127aeff70fe371fa3091777c4a50f037682f
afurourrego <infoafuro@gmail.com>
2020-02-07 15:26:35 -0500

    add ability n rules for role super_admin

---
+++ app/models/ability.rb
@@ -0,0 +1,33 @@
+class Ability
+  include CanCan::Ability
+  include Levels::SuperAdmin
+
+  def initialize(user)
+    @user = user
+    user ? user_rules : guest_user_rules
+  end
+
+  def user_rules
+    return exec_role_rules(@user.role) if @user.role
+  end
+
+  def exec_role_rules(role)
+    meth = :"#{role}_rules"
+    send(meth) if respond_to? meth
+  end
+
+  def guest_user_rules
+    cannot :manage, User
+  end
+
+  def as_json
+    rules.map do |rule|
+      {
+        base_behavior: rule.base_behavior,
+        actions: rule.actions.as_json,
+        subjects: rule.subjects.map(&:to_s),
+        conditions: rule.conditions.as_json
+      }
+    end
+  end
+end
---
+++ app/models/levels/super_admin.rb
@@ -0,0 +1,11 @@
+module Levels
+  module SuperAdmin
+    def super_admin_rules
+      if @user.level == 1
+        can :manage, :all
+      else
+        cannot :manage, :all
+      end
+    end
+  end
+end
