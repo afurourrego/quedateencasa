f3c271ade8260b4b304d30612a39808974a71b39
capjuancode <capjuancode@gmail.com>
2020-02-07 21:41:39 -0800

    fix error call not initiated

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Initialize the Rails  error handler for materilizecss message
+require_relative 'customize_error'
