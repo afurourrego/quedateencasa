ddfa86a515a0b9d061e7651175326aab2629bad2
afurourrego <infoafuro@gmail.com>
2020-02-11 13:06:52 -0500

    added error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+  include ErrorResponseActions
+  rescue_from CanCan::AccessDenied, with: :authorization_error
+  rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
+
   private
 
   def set_current_user
