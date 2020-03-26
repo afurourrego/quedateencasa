615fe44a586df538b615ab5b46fe98305355a9ce
afurourrego <infoafuro@gmail.com>
2020-03-25 21:26:07 -0500

    update project name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "quedateencasa_production"
+  # config.active_job.queue_name_prefix = "QuedateEnCasa_production"
 
   config.action_mailer.perform_caching = false
 
