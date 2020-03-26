394af8ffbb5b79194f0bfe6ed0745f3b950be2d4
afurourrego <infoafuro@gmail.com>
2020-03-26 01:48:16 -0500

    update company rules

--- app/models/levels/company.rb
+++ app/models/levels/company.rb
@@ -2,8 +2,8 @@ module Levels
   module Company
     def company_rules
       if @user.level == 1
-        can  %i[show update],         User, id: @user.id
-        can  %i[create update],       Location, user_id: @user.id
+        can  %i[show update],                 User, id: @user.id
+        can  %i[create show update destroy],  Location, user_id: @user.id
       else
         cannot :manage, :all
       end
