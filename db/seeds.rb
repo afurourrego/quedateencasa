e558ae9ac43040ed96d317791b96a41025a9ab61
afurourrego <infoafuro@gmail.com>
2020-03-26 01:07:12 -0500

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
 p 'Seed User'
-['capjuancode@tawa.com', 'dayeane@devappstudio.com', 'cristhian.u@devappstudio.com'].each do |email|
+['cristhian.u@devappstudio.com'].each do |email|
   User.new(email: email,
            password: '123456',
            password_confirmation: '123456',
            role: 'super_admin',
+           level: 1).save(validate: false)
+end
+['company@covii.com'].each do |email|
+  User.new(email: email,
+           password: '123456',
+           password_confirmation: '123456',
+           role: 'company',
            level: 1).save(validate: false)
 end
 
