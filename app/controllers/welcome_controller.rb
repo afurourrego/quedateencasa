e8a032204469236198972a501fcf72cad8adb164
afurourrego <infoafuro@gmail.com>
2020-03-27 17:10:35 -0500

    header count

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,8 @@ class WelcomeController < ApplicationController
   def home
     @locations = Location.search(location_params_search).joins(:city).merge(City.order(name: :asc)).order('category ASC, name ASC')
     @locations = @locations.page(params[:page])
+    @locations_count = Location.count
+    @cities_locations = Location.pluck(:city_id).uniq.count
   end
 
   private
